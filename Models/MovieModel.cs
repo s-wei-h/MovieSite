5b313aadf994b5441ac74a89849d821de6d98155
Sarah Huang <swei0815@byu.edu>
2021-02-04 16:50:08 +0800

    Add project files.

---
+++ Models/MovieModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieSite.Models
+{
+    public class MovieModel
+    {
+        public string title { get; set; }
+        public string category { get; set; }
+        public string year { get; set; }
+        public string director { get; set; }
+        public string rating { get; set; }
+        public bool edited { get; set; }
+        public string lent_to { get; set; }
+        public string notes { get; set; }
+
+    }
+}
---
+++ Models/TempStorage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieSite.Models
+{
+    public class TempStorage
+    {
+        private static List<MovieModel> MovieList = new List<MovieModel>();
+
+        public static IEnumerable<MovieModel> Movies => MovieList;
+
+        public static void addMovie(MovieModel newMovie)
+        {
+            MovieList.Add(newMovie);
+        }
+    }
+}
